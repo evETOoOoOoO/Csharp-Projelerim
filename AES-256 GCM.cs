e2517dc421821462ff346913582d407d21a7f081
evETOoOoOoO <ahmetasafgulec@gmail.com>
2026-04-25 14:29:21 +0300

    .
    .

---
+++ AES-256 GCM.cs
@@ -0,0 +1,14 @@
+//AES-256 GCM ŞİFRELEME
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+    }
+}
